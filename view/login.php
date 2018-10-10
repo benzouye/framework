4059931b4eccc95c0d12b9622f9e987fb63bf1f1
benzouye <benzouye@hotmail.com>
2018-10-10 16:39:53 +0200

    Boostrap 4 migration

--- view/login.php
+++ view/login.php
@@ -1,6 +1,6 @@
 
 				<div class="row">
-					<div class="col-sm-3 col-sm-offset-3">
+					<div class="col-sm-4">
 						<p>Veuillez vous connecter.</p>
 						<form method="POST" action="index.php">
 							<div class="form-group">
@@ -9,8 +9,7 @@
 							<div class="form-group">
 								<input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
 							</div>
-							<button type="submit" class="btn btn-secondary">Se connecter</button>
+							<button type="submit" class="btn btn-success">Se connecter</button> Pas encore inscrit ? <a class="btn btn-secondary" href="index.php?item=register">Inscrivez-vous</a>
 						</form>
-						<p>Pas encore inscrit ? <a href="index.php?item=register">Inscrivez-vous</a> ...</p>
 					</div>
 				</div>
