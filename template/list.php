85c1f2e6dea4f4c279a2c1c3f4ce046acfb299d9
benzouye <benzouye@hotmail.com>
2018-10-31 18:00:57 +0100

    Add table-sm class

--- template/list.php
+++ template/list.php
@@ -34,7 +34,7 @@
 		if( count($items)>0 ) {
 ?>
 			<div class="table-responsive">
-			<table class="table table-striped table-hover table-bordered">
+			<table class="table table-sm table-striped table-hover table-bordered">
 				<thead>
 					<tr>
 						<th width="50">Ouvrir</th>
