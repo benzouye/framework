9aa0222e932857a20b24529d3e0f9c06f676ce1b
benzouye <benzouye@hotmail.com>
2018-10-31 16:45:49 +0100

    Add MySQL Locale support

--- config.php
+++ config.php
@@ -112,11 +112,14 @@
 	define( 'ERRMOD' , PDO::ERRMODE_EXCEPTION );
 	// Fetch mode
 	define( 'FETMOD' , PDO::FETCH_OBJ );
+	// Time locale
+	define( 'DBLOCALE' , 'fr_FR' );
 	
 	/*
 	*	Connexion BDD
 	*/
 	try {
+		$pdoOptions[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET lc_time_names = "'.DBLOCALE.'"';
 		$pdoOptions[PDO::ATTR_ERRMODE] = ERRMOD;
 		$pdoOptions[PDO::ATTR_DEFAULT_FETCH_MODE] = FETMOD;
 		$bdd = new PDO(DBTYPE.':host='.DBHOST.';dbname='.DBNAME.';charset='.DBCHAR, DBUSER, DBPASS, $pdoOptions);
