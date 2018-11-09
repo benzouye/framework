d8bd541cbfbe18ba6a94f64790b95e4154e88413
benzouye <benzouye@hotmail.com>
2018-11-09 17:59:52 +0100

    Add chartJS homepage widget handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,34 @@
-// Paramètres régionaux du sélecteur de date
-$( ".datepicker" ).datepicker({
-	firstDay: 1,
-	closeText: 'Fermer',
-	prevText: 'Précédent',
-	nextText: 'Suivant',
-	currentText: 'Aujourd\'hui',
-	monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-	monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-	dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-	dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
-	dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
-	weekHeader: 'Sem.',
-	dateFormat: "yy-mm-dd"
-});
-
-// Sélecteur de couleur
-$( ".colorpicker" ).colorpicker();
-
-// Affichage du formulaire de changement de mot de passe
-$( ".update-password" ).change( function(e){
-	var templateInput = '<input required type="password" placeholder="Nouveau mot de passe" name="'+$(this).data('name')+'" class="form-control">';
-	if( $(this).prop('checked') == true ) {
-		$(templateInput).insertAfter(this);
-	} else {
-		$(this).next().remove();
-	}
-});
-
 // Après chargement de la page
 $( document ).ready( function(){
+	// Paramètres régionaux du sélecteur de date
+	$( ".datepicker" ).datepicker({
+		firstDay: 1,
+		closeText: 'Fermer',
+		prevText: 'Précédent',
+		nextText: 'Suivant',
+		currentText: 'Aujourd\'hui',
+		monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+		monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+		dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+		dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+		dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+		weekHeader: 'Sem.',
+		dateFormat: "yy-mm-dd"
+	});
+
+	// Sélecteur de couleur
+	$( ".colorpicker" ).colorpicker();
+
+	// Affichage du formulaire de changement de mot de passe
+	$( ".update-password" ).change( function(e){
+		var templateInput = '<input required type="password" placeholder="Nouveau mot de passe" name="'+$(this).data('name')+'" class="form-control">';
+		if( $(this).prop('checked') == true ) {
+			$(templateInput).insertAfter(this);
+		} else {
+			$(this).next().remove();
+		}
+	});
+	
 	// Reload après print
 	$('.btnReload').click( function() {
 		window.location.reload();
@@ -101,138 +101,50 @@ $( document ).ready( function(){
 			}
 		});
 	});
-
-// Custom combobox
-$( function() {
-		$.widget( "custom.combobox", {
-			_create: function() {
-				this.wrapper = $( "<span>" )
-					.addClass( "custom-combobox" )
-					.insertAfter( this.element );
- 
-				this.element.hide();
-				this._createAutocomplete();
-				this._createShowAllButton();
-			},
- 
-			_createAutocomplete: function() {
-				var selected = this.element.children( ":selected" ),
-					value = selected.val() ? selected.text() : "";
- 
-				this.input = $( "<input>" )
-					.appendTo( this.wrapper )
-					.val( value )
-					.attr( "title", "" )
-					.addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
-					.autocomplete({
-						delay: 0,
-						minLength: 0,
-						source: $.proxy( this, "_source" )
-					})
-					.tooltip({
-						classes: {
-							"ui-tooltip": "ui-state-highlight"
-						}
-					});
- 
-				this._on( this.input, {
-					autocompleteselect: function( event, ui ) {
-						ui.item.option.selected = true;
-						this._trigger( "select", event, {
-							item: ui.item.option
-						});
-					},
- 
-					autocompletechange: "_removeIfInvalid"
-				});
-			},
- 
-			_createShowAllButton: function() {
-				var input = this.input,
-					wasOpen = false;
- 
-				$( "<a>" )
-					.attr( "tabIndex", -1 )
-					.tooltip()
-					.appendTo( this.wrapper )
-					.button({
-						icons: {
-							primary: "ui-icon-triangle-1-s"
-						},
-						text: false
-					})
-					.removeClass( "ui-corner-all" )
-					.addClass( "custom-combobox-toggle ui-corner-right" )
-					.on( "mousedown", function() {
-						wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-					})
-					.on( "click", function() {
-						input.trigger( "focus" );
- 
-						// Close if already visible
-						if ( wasOpen ) {
-							return;
-						}
- 
-						// Pass empty string as value to search for, displaying all results
-						input.autocomplete( "search", "" );
-					});
-			},
- 
-			_source: function( request, response ) {
-				var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-				response( this.element.children( "option" ).map(function() {
-					var text = $( this ).text();
-					if ( this.value && ( !request.term || matcher.test(text) ) )
-						return {
-							label: text,
-							value: text,
-							option: this
-						};
-				}) );
-			},
- 
-			_removeIfInvalid: function( event, ui ) {
- 
-				// Selected an item, nothing to do
-				if ( ui.item ) {
-					return;
-				}
- 
-				// Search for a match (case-insensitive)
-				var value = this.input.val(),
-					valueLowerCase = value.toLowerCase(),
-					valid = false;
-				this.element.children( "option" ).each(function() {
-					if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-						this.selected = valid = true;
-						return false;
+	
+	// Graphique ChartJS
+	if( $(".homepage-chart").length ) {
+		$(".homepage-chart").each( function(e) {
+			
+			var canvas = $(this);
+			
+			$.ajax({
+				method: "GET",
+				url: "ajax.php",
+				data: {
+					parent_item: "analyse",
+					parent_id: $(this).data("analyse"),
+					object: true
+				},
+				dataType: 'json',
+				success: function( analyse ) {
+					let chartLabels = [];
+					let chartValues = [];
+					let chartOptions = JSON.parse( analyse.object.options );
+					
+					for( item in analyse.data ) {
+						chartLabels.push( analyse.data[item].Label );
+						chartValues.push( analyse.data[item].Value );
 					}
-				});
- 
-				// Found a match, nothing to do
-				if ( valid ) {
-					return;
+					
+					let graphOptions = {
+						type: chartOptions.type,
+						data:{
+							labels: chartLabels,
+							datasets:[
+								{
+									label: analyse.object.description,
+									data: chartValues,
+									backgroundColor: chartOptions.backgroundColor,
+								}
+							]
+						},
+						options: chartOptions.options
+					};
+					
+					let graphique = new Chart( canvas, graphOptions );
 				}
- 
-				// Remove invalid value
-				this.input
-					.val( "" )
-					.attr( "title", value + " didn't match any item" )
-					.tooltip( "open" );
-				this.element.val( "" );
-				this._delay(function() {
-					this.input.tooltip( "close" ).attr( "title", "" );
-				}, 2500 );
-				this.input.autocomplete( "instance" ).term = "";
-			},
- 
-			_destroy: function() {
-				this.wrapper.remove();
-				this.element.show();
-			}
+			});
 		});
- 
-		$( ".auto-complete" ).combobox();
-	});
+	}
 });
