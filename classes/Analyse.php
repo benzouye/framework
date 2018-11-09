ac9cd5b7dbac8c8c3f9a5fd39ea30b7a57688780
benzouye <benzouye@hotmail.com>
2018-11-09 14:10:51 +0100

    Delete type method

--- classes/Analyse.php
+++ classes/Analyse.php
@@ -1,36 +1,6 @@
 <?php
 class analyse extends Model {
 	
-	public function setItem( $data, $register = false ) {
-		parent::setItem( $data, $register );
-	}
-	
-	public function getTypeAnalyse() {
-		$type = '';
-		try {
-			$requete = $this->bdd->query('
-				SELECT TA.classe
-				FROM '.DBPREF.'type_analyse TA
-					INNER JOIN '.DBPREF.'analyse A
-						ON TA.id_type_analyse = A.id_type_analyse
-				WHERE A.id_analyse = '.$this->id
-			);
-			$type = $requete->fetchColumn();
-			$requete->closeCursor();
-		}
-		catch( Exception $e ) {
-			if( $this->manager->getDebug() ) {
-				$msg = $e->getMessage();
-			} else {
-				$msg = 'type analyse';
-			}
-			$this->manager->setError( sprintf( M_ITEMSERR, $msg ) );
-		}
-		finally {
-			return $type;	
-		}
-	}
-	
 	public function getDatas() {
 		$datas = array();
 		try {
