f7d59ddfb737e46d5802b44a9cee1c588eee84e5
benzouye <benzouye@hotmail.com>
2018-11-20 09:19:09 +0100

    Add document link

--- init_db.sql
+++ init_db.sql
@@ -38,6 +38,7 @@ CREATE TABLE `prefix_document` (
   `id_document` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `fichier` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `id_type_document` int(11) unsigned NOT NULL,
+  `id_exemple` int(10) unsigned NOT NULL,
   `user_cre` int(11) NOT NULL,
   `date_cre` datetime NOT NULL,
   `user_maj` int(11) DEFAULT NULL,
@@ -46,9 +47,11 @@ CREATE TABLE `prefix_document` (
   KEY `FK_document_id_type_document` (`id_type_document`),
   KEY `user_cre` (`user_cre`),
   KEY `user_maj` (`user_maj`),
-  CONSTRAINT `FK_document_id_type_document` FOREIGN KEY (`id_type_document`) REFERENCES `prefix_type_document` (`id_type_document`),
+  KEY `id_exemple` (`id_exemple`),
   CONSTRAINT `prefix_document_ibfk_1` FOREIGN KEY (`user_cre`) REFERENCES `prefix_utilisateur` (`id_utilisateur`),
-  CONSTRAINT `prefix_document_ibfk_2` FOREIGN KEY (`user_maj`) REFERENCES `prefix_utilisateur` (`id_utilisateur`)
+  CONSTRAINT `prefix_document_ibfk_2` FOREIGN KEY (`user_maj`) REFERENCES `prefix_utilisateur` (`id_utilisateur`),
+  CONSTRAINT `prefix_document_ibfk_3` FOREIGN KEY (`id_exemple`) REFERENCES `prefix_exemple` (`id_exemple`),
+  CONSTRAINT `prefix_document_ibfk_4` FOREIGN KEY (`id_type_document`) REFERENCES `prefix_type_document` (`id_type_document`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
@@ -72,6 +75,29 @@ INSERT INTO `prefix_etat` (`id_etat`, `libelle`, `user_cre`, `date_cre`, `user_m
 (1,	'En attente',	1,	'2017-11-06 12:49:15',	NULL,	NULL),
 (2,	'En cours',	1,	'2017-11-22 13:37:33',	NULL,	NULL),
 (3,	'Terminé',	1,	'2017-11-22 13:37:41',	NULL,	NULL);
+
+DROP TABLE IF EXISTS `prefix_exemple`;
+CREATE TABLE `prefix_exemple` (
+  `id_exemple` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `montant` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `fichier` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `localisation` text COLLATE utf8_unicode_ci,
+  `id_etat` int(11) unsigned NOT NULL DEFAULT '1',
+  `user_cre` int(11) NOT NULL,
+  `date_cre` datetime NOT NULL,
+  `user_maj` int(11) DEFAULT NULL,
+  `date_maj` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_exemple`),
+  KEY `id_etat` (`id_etat`),
+  KEY `user_cre` (`user_cre`),
+  KEY `user_maj` (`user_maj`),
+  CONSTRAINT `prefix_exemple_ibfk_1` FOREIGN KEY (`id_etat`) REFERENCES `prefix_etat` (`id_etat`),
+  CONSTRAINT `prefix_exemple_ibfk_2` FOREIGN KEY (`user_cre`) REFERENCES `prefix_utilisateur` (`id_utilisateur`),
+  CONSTRAINT `prefix_exemple_ibfk_3` FOREIGN KEY (`user_maj`) REFERENCES `prefix_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 DROP TABLE IF EXISTS `prefix_groupe`;
 CREATE TABLE `prefix_groupe` (
@@ -103,19 +129,19 @@ CREATE TABLE `prefix_groupe_item` (
   PRIMARY KEY (`id_groupe`,`alias`),
   KEY `alias` (`alias`),
   KEY `id_groupe` (`id_groupe`),
-  CONSTRAINT `prefix_groupe_item_ibfk_1` FOREIGN KEY (`id_groupe`) REFERENCES `prefix_groupe` (`id_groupe`) ON DELETE CASCADE,
-  CONSTRAINT `groupe_item_alias_fk` FOREIGN KEY (`alias`) REFERENCES `prefix_item` (`alias`)
+  CONSTRAINT `groupe_item_alias_fk` FOREIGN KEY (`alias`) REFERENCES `prefix_item` (`alias`),
+  CONSTRAINT `prefix_groupe_item_ibfk_1` FOREIGN KEY (`id_groupe`) REFERENCES `prefix_groupe` (`id_groupe`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `prefix_groupe_item` (`id_groupe`, `alias`, `create`, `read`, `update`, `delete`) VALUES
-(2,	'analyse',	1,	1,	1,	1),
-(2,	'document',	1,	1,	1,	1),
-(2,	'home',	0,	1,	0,	0),
-(2,	'logout',	0,	1,	0,	0),
 (1,	'analyse',	0,	1,	0,	0),
 (1,	'document',	0,	1,	0,	0),
 (1,	'home',	0,	1,	0,	0),
-(1,	'logout',	0,	1,	0,	0);
+(1,	'logout',	0,	1,	0,	0),
+(2,	'analyse',	1,	1,	1,	1),
+(2,	'document',	1,	1,	1,	1),
+(2,	'home',	0,	1,	0,	0),
+(2,	'logout',	0,	1,	0,	0);
 
 DROP TABLE IF EXISTS `prefix_item`;
 CREATE TABLE `prefix_item` (
@@ -146,14 +172,15 @@ INSERT INTO `prefix_item` (`id_item`, `alias`, `nom`, `description`, `static`, `
 (50,	'analyse',	'Analyses',	'Les différentes extractions possibles',	0,	1,	1,	0,	1,	200,	'chart-bar',	1,	'2018-10-29 18:20:27',	1,	'2018-10-31 17:24:48'),
 (40,	'document',	'Documents',	'Les documents liés aux différents éléments',	0,	1,	1,	0,	0,	0,	'file-alt',	1,	'2017-11-06 11:11:49',	1,	'2017-11-13 15:18:25'),
 (32,	'etat',	'Etats',	'Les états d\'inscription possibles',	0,	0,	1,	1,	5,	3,	'check',	1,	'2017-11-02 16:55:34',	1,	'2017-11-02 17:22:06'),
+(51,	'exemple',	'Exemples',	'Un objet exemple',	0,	1,	1,	0,	1,	20,	'question',	1,	'2018-11-09 18:20:35',	1,	'2018-11-19 09:55:00'),
 (26,	'groupe',	'Groupes',	'Groupes d\'utilisateurs',	0,	1,	1,	1,	9,	5,	'users',	1,	'2017-10-24 07:47:06',	1,	'2018-11-05 09:37:48'),
-(4,	'home',	'Accueil',	'Les comptes en bref',	1,	0,	1,	0,	1,	10,	'home',	1,	'2017-10-24 07:47:06',	1,	'2018-10-31 17:38:33'),
+(4,	'home',	'Accueil',	'Bienvenue dans le framework !',	1,	0,	1,	0,	1,	10,	'home',	1,	'2017-10-24 07:47:06',	1,	'2018-10-31 17:38:33'),
 (5,	'item',	'Eléments',	'Les éléments de construction du site',	0,	0,	1,	1,	9,	3,	'list-alt',	1,	'2017-10-24 07:47:06',	1,	'2017-10-24 13:56:49'),
 (6,	'logout',	'Déconnexion',	'Déconnexion',	1,	0,	1,	0,	10,	2,	'sign-out-alt',	1,	'2017-10-24 07:47:06',	1,	'2018-10-29 14:58:51'),
 (8,	'option',	'Configuration',	'Les options du site',	0,	0,	1,	1,	9,	4,	'cog',	1,	'2017-10-24 07:47:06',	1,	'2017-10-24 13:56:42'),
 (27,	'register',	'Enregistrement',	'Formulaire de création de compte',	1,	0,	1,	1,	0,	0,	'check',	1,	'2017-10-25 14:02:39',	1,	'2018-10-29 14:59:08'),
-(12,	'utilisateur',	'Utilisateurs',	'Les utilisateurs de l\'application',	0,	1,	1,	1,	9,	6,	'user',	1,	'2017-10-24 07:47:06',	NULL,	NULL),
-(51,	'exemple',	'Exemples',	'Un objet exemple',	0,	0,	1,	0,	1,	20,	'question',	1,	'2018-11-09 18:20:35',	NULL,	NULL);
+(52,	'type_document',	'Types de document',	'Les différents type de document possibles',	0,	0,	1,	0,	5,	100,	'file',	1,	'2018-11-19 10:01:11',	NULL,	NULL),
+(12,	'utilisateur',	'Utilisateurs',	'Les utilisateurs de l\'application',	0,	1,	1,	1,	9,	6,	'user',	1,	'2017-10-24 07:47:06',	NULL,	NULL);
 
 DROP TABLE IF EXISTS `prefix_option`;
 CREATE TABLE `prefix_option` (
@@ -199,7 +226,8 @@ INSERT INTO `prefix_type_analyse` (`id_type_analyse`, `libelle`, `user_cre`, `da
 (1,	'Tableau droit',	1,	'2018-01-03 11:47:07',	NULL,	NULL),
 (2,	'Tableau croisé',	1,	'2018-01-03 11:47:22',	NULL,	NULL),
 (3,	'Valeur unique',	1,	'2018-01-03 11:47:22',	NULL,	NULL),
-(4,	'Graphique',	1,	'2018-11-02 15:24:27',	NULL,	NULL);
+(4,	'Graphique',	1,	'2018-11-02 15:24:27',	NULL,	NULL),
+(5,	'Carte',	1,	'2018-11-19 16:34:10',	NULL,	NULL);
 
 DROP TABLE IF EXISTS `prefix_type_document`;
 CREATE TABLE `prefix_type_document` (
@@ -244,27 +272,3 @@ CREATE TABLE `prefix_utilisateur` (
 
 INSERT INTO `prefix_utilisateur` (`id_utilisateur`, `identifiant`, `password`, `email`, `admin`, `id_groupe`, `token`, `valide`, `user_cre`, `date_cre`, `user_maj`, `date_maj`) VALUES
 (1,	'admin',	'$2y$10$2CrYdHmPrL1wqiWsGCNoguvVTra.ALW5/6vjH3ofodG7JW1QBEXrK',	'admin@mydomain.com',	1,	2,	'',	1,	1,	'2017-10-24 07:47:06',	1,	'2018-11-08 13:37:37');
-
-
-DROP TABLE IF EXISTS `prefix_exemple`;
-CREATE TABLE `prefix_exemple` (
-  `id_exemple` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `libelle` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `montant` decimal(10,2) NOT NULL DEFAULT '0',
-  `image` varchar(60) DEFAULT NULL,
-  `fichier` varchar(60) DEFAULT NULL,
-  `id_etat` int(11) unsigned NOT NULL DEFAULT '1',
-  `user_cre` int(11) NOT NULL,
-  `date_cre` datetime NOT NULL,
-  `user_maj` int(11) DEFAULT NULL,
-  `date_maj` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_exemple`),
-  KEY `id_etat` (`id_etat`),
-  KEY `user_cre` (`user_cre`),
-  KEY `user_maj` (`user_maj`),
-  CONSTRAINT `prefix_exemple_ibfk_1` FOREIGN KEY (`id_etat`) REFERENCES `prefix_etat` (`id_etat`),
-  CONSTRAINT `prefix_exemple_ibfk_2` FOREIGN KEY (`user_cre`) REFERENCES `prefix_utilisateur` (`id_utilisateur`),
-  CONSTRAINT `prefix_exemple_ibfk_3` FOREIGN KEY (`user_maj`) REFERENCES `prefix_utilisateur` (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- 2018-11-08 12:37:51
