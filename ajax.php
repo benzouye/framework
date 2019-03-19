11d70c90839eded33cbc73e9ad71b4f561d4cea0
benzouye <benzouye@hotmail.com>
2019-03-19 17:45:05 +0100

    Secure access without authentication

--- ajax.php
+++ ajax.php
@@ -1,58 +1,65 @@
 <?php
+	// Initialisation
+	session_start();
 	require_once( 'config.php' );
-	// Retour par défaut
-	$retour = array(
-		'message' => 'Bad Request, missing parameters',
-		'item' => false,
-		'data' => false,
-	);
+	$manager = new Manager( $bdd, $debug );
+	$user = $manager->getUser();
+	$retour = array( 'message' => 'Unauthorized user' );
 	
-	if( isset( $_GET['ajaxGet'], $_GET['parent_item'] ) ) {
-		$item = $_GET['parent_item'];
-		$id = isset( $_GET['parent_id'] ) ? intval( $_GET['parent_id'] ) : 0;
-		
-		$manager = new Manager( $bdd, $debug );
-		$model = ${'model_'.$item};
-		$nomClasse = ucfirst($item);
-		
-		switch( $_GET['ajaxGet'] ) {
-			case 'relation' :
-				if( isset( $_GET['relation'] ) ) {
-					$object = new $nomClasse( $bdd, $manager, $model );
-					$relation = $_GET['relation'];
-					$retour['message'] = 'Relation between '.$item.' and '.$relation;
-					
-					if( isset( $_GET['item_id'] ) ) {
-						$retour['data'] = $object->{'save_'.$relation}( $_GET );
-					} else {
-						$retour['data'] = $object->{'get_'.$relation.'_dispo'}( $_GET );
+	if( $user ) {
+		// Retour par défaut
+		$retour = array(
+			'message' => 'Bad Request, missing parameters',
+			'item' => false,
+			'data' => false,
+		);
+	
+		if( isset( $_GET['ajaxGet'], $_GET['parent_item'] ) ) {
+			$item = $_GET['parent_item'];
+			$id = isset( $_GET['parent_id'] ) ? intval( $_GET['parent_id'] ) : 0;
+			
+			$model = ${'model_'.$item};
+			$nomClasse = ucfirst($item);
+			
+			switch( $_GET['ajaxGet'] ) {
+				case 'relation' :
+					if( isset( $_GET['relation'] ) ) {
+						$object = new $nomClasse( $bdd, $manager, $model );
+						$relation = $_GET['relation'];
+						$retour['message'] = 'Relation between '.$item.' and '.$relation;
+						
+						if( isset( $_GET['item_id'] ) ) {
+							$retour['data'] = $object->{'save_'.$relation}( $_GET );
+						} else {
+							$retour['data'] = $object->{'get_'.$relation.'_dispo'}( $_GET );
+						}
 					}
-				}
-				break;
-			case 'analyse' :
-				if( $id ) {
+					break;
+				case 'analyse' :
+					if( $id ) {
+						$object = new $nomClasse( $bdd, $manager, $model );
+						$retour['message'] = 'Values for analyse '.$id;
+						$retour['item'] = $object->getItem( $id );
+						$retour['data'] = $object->getDatas();
+					}
+					break;
+				case 'schedule' :
 					$object = new $nomClasse( $bdd, $manager, $model );
-					$retour['message'] = 'Values for analyse '.$id;
-					$retour['item'] = $object->getItem( $id );
-					$retour['data'] = $object->getDatas();
-				}
-				break;
-			case 'schedule' :
-				$object = new $nomClasse( $bdd, $manager, $model );
-				$retour = $object->get_scheduler( $_GET );
-				break;
-			case 'distinct' :
-				if( isset( $_GET['colonne'], $_GET['term'] ) ) {
-					$object = new Model( $bdd, $manager, $model );
-					$retour = $object->getDistinctValues( $_GET['colonne'], $_GET['term'] );
-				}
-				break;
-			case 'option' :
-				if( isset( $_GET['alias'] ) ) {
-					$retour['message'] = 'Value for option '.$_GET['alias'];
-					$retour['data'] = $manager->getOption( $_GET['alias'] );
-				}
-				break;
+					$retour = $object->get_scheduler( $_GET );
+					break;
+				case 'distinct' :
+					if( isset( $_GET['colonne'], $_GET['term'] ) ) {
+						$object = new Model( $bdd, $manager, $model );
+						$retour = $object->getDistinctValues( $_GET['colonne'], $_GET['term'] );
+					}
+					break;
+				case 'option' :
+					if( isset( $_GET['alias'] ) ) {
+						$retour['message'] = 'Value for option '.$_GET['alias'];
+						$retour['data'] = $manager->getOption( $_GET['alias'] );
+					}
+					break;
+			}
 		}
 	}
 	
