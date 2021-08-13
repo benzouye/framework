3aea5405a3f140815f1a1ae9e784bb5e99b3a04f
benzouye <benzouye@hotmail.com>
2021-08-13 11:32:45 +0200

    Add files via upload

---
+++ items/ModelAffectation.php
@@ -0,0 +1,37 @@
+<?php
+	$model_affectation = (object) array(
+		'itemName' => 'affectation',
+		'table' => DBPREF.'affectation',
+		'single' => 'affectation',
+		'plural' => 'affectations',
+		'orderby' => 'libelle',
+		'columns' => array(
+			(object) array(
+				'name' => 'id_affectation',
+				'nicename' => 'ID',
+				'grid' => (object) array(
+					'div' => 12,
+					'label' => 2,
+					'value' => 1,
+				),
+				'params' => array( 'type' => 'number', 'step' => 1 ),
+				'visible' => false,
+				'editable' => false,
+				'required' => false
+			),
+			(object) array(
+				'name' => 'libelle',
+				'nicename' => 'Libellé',
+				'grid' => (object) array(
+					'div' => 12,
+					'label' => 2,
+					'value' => 4,
+				),
+				'params' => array( 'type' => 'text' ),
+				'visible' => true,
+				'editable' => true,
+				'required' => true
+			)
+		)
+	);
+?>
