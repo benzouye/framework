bb4754828ff5ec46a66660bcaefc267e01ea34ff
benzouye <benzouye@hotmail.com>
2021-08-13 14:19:03 +0200

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -10,15 +10,13 @@
 			</div>
 		</div>
 
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/jquery.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
 		<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/moment.min.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/Chart.min.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/fullcalendar.min.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/scheduler.min.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/leaflet.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/fr.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.9.0/main.min.js"></script>
+		<script type="text/javascript" src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/main.js"></script>
 	</body>
 </html>
