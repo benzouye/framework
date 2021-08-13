e21948463bebad7d5df8b158aa37fc0f68fdacb6
benzouye <benzouye@hotmail.com>
2021-08-13 11:19:42 +0200

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -10,17 +10,15 @@
 			</div>
 		</div>
 
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/jquery.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/jquery.js"></script>
 		<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/popper.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/bootstrap.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/bootstrap-colorpicker.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/moment.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/Chart.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/fullcalendar.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/scheduler.min.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/leaflet.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/fr.js"></script>
-		<script type="text/javascript" src="<?php echo SITEURL; ?>assets/js/main.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/moment.min.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/Chart.min.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/fullcalendar.min.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/scheduler.min.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/leaflet.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/fr.js"></script>
+		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/main.js"></script>
 	</body>
 </html>
