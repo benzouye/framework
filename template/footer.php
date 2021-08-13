c11506d209ed7e38a90de8f2026fa75b9dca9e2d
benzouye <benzouye@hotmail.com>
2021-08-13 14:22:48 +0200

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -10,7 +10,7 @@
 			</div>
 		</div>
 
-		<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 		<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
