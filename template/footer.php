970b3e1b30cba0b927083ca38c56e7871e2a7ccc
benzouye <benzouye@hotmail.com>
2021-08-13 16:10:59 +0200

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -16,8 +16,8 @@
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.9.0/main.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.9.0/locales/fr.js"></script>
 		<script type="text/javascript" src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/main.js"></script>
-		<script type="text/javascript" src="<?=SITEURL; ?>assets/js/fr.js"></script>
 	</body>
 </html>
