1978445039337b9aa75abb6a03d34f354d91297f
benzouye <benzouye@hotmail.com>
2021-08-13 11:31:58 +0200

    Update Utilisateur.php

--- classes/Utilisateur.php
+++ classes/Utilisateur.php
@@ -25,6 +25,106 @@ class utilisateur extends Model {
 		}
 	}
 	
+	public function get_affectation() {
+		try {
+			$requete = $this->bdd->query('
+				SELECT A.*
+				FROM '.DBPREF.'utilisateur_affectation UA
+					INNER JOIN '.DBPREF.'affectation A
+						ON UA.id_affectation = A.id_affectation
+				WHERE UA.id_utilisateur = '.intval( $this->id ).'
+				ORDER BY A.libelle ASC;'
+			);
+			$this->users = $requete->fetchAll();
+		}
+		catch( Exception $e ) {
+			if( $this->manager->getDebug() ) {
+				$msg = $e->getMessage();
+			} else {
+				$msg = 'affectation';
+			}
+			$this->manager->setError( sprintf( M_ITEMSERR, $msg ) );
+		}
+		finally {
+			return $this->users;	
+		}
+	}
+	
+	public function get_affectation_dispo() {
+		try {
+			$requete = $this->bdd->query('
+				SELECT
+					A.id_affectation AS id,
+					A.libelle AS nom,
+					IF( UA.id_affectation IS NULL, 0, 1 ) AS active
+				FROM
+					'.DBPREF.'affectation A
+						LEFT JOIN '.DBPREF.'utilisateur_affectation UA
+							ON UA.id_affectation = A.id_affectation
+							AND UA.id_utilisateur = '.intval( $_GET['parent_id'] ).'
+				ORDER BY A.libelle ASC;'
+			);
+			$this->users = $requete->fetchAll();
+		}
+		catch( Exception $e ) {
+			if( $this->manager->getDebug() ) {
+				$msg = $e->getMessage();
+			} else {
+				$msg = 'affectation';
+			}
+			$this->manager->setError( sprintf( M_ITEMSERR, $msg ) );
+		}
+		finally {
+			return $this->users;	
+		}
+	}
+	
+	public function set_affectation( $data ) {
+		try {	
+			$utilisateur = $data['id'];
+			
+			$this->bdd->query( '
+				DELETE FROM '.DBPREF.'utilisateur_affectation
+				WHERE id_utilisateur = '.intval($utilisateur).';'
+			);
+			
+			if( isset( $data['affectation'] ) ) {
+				$requete = $this->bdd->prepare('
+					INSERT INTO '.DBPREF.'utilisateur_affectation ( id_utilisateur, id_affectation )
+					VALUES ( ?, ? );'
+				);
+				foreach( $data['affectation'] as $affectation ) {
+					$requete->execute( [ $utilisateur, $affectation ] );
+					$requete->closeCursor();
+				}
+			}
+			$this->manager->setMessage( 'Les affectations ont bien été mis à jour pour cet utilisateur' );
+		}
+		catch( Exception $e ) {
+			if( $this->manager->getDebug() ) {
+				$msg = $e->getMessage();
+			} else {
+				$msg = 'groupe';
+			}
+			$this->manager->setError( sprintf( M_RELNEWERR, $msg, 'accès' ) );
+		}
+	}
+	
+	public function del_affectation( $data ) {
+		try {
+			$requete = $this->bdd->prepare('
+				DELETE FROM '.DBPREF.'utilisateur_affectation
+				WHERE id_utilisateur = ?
+				AND id_affectation = ?;'
+			);
+			$requete->execute( array( $data['id'], $data['rel_id'] ) );
+			$requete->closeCursor();
+		}
+		catch( Exception $e ) {
+			$this->manager->setError( sprintf( M_RELDELERR, 'utilisateur', 'affectation' ) );
+		}
+	}
+	
 	public function valideEmailUser( $identifiant, $token ) {
 		try {
 			$requete = $this->bdd->prepare('
@@ -59,4 +159,3 @@ class utilisateur extends Model {
 		}
 	}
 }
-?>
