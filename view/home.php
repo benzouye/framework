3d520faf482acdfd0d9330fa00e9088ef297e7d9
benzouye <benzouye@hotmail.com>
2021-08-23 13:52:05 +0200

    Update home.php

--- view/home.php
+++ view/home.php
@@ -1,16 +1,14 @@
 <?php
 	if( !$user ) {
-		// On affiche le formulaire de connexion
 		include( VIEWDIR.'login.php' );
 	} else {
-		// Sinon on affiche la page d'accueil
 ?>
 				<div class="row">
 <?php
 		echo $manager->getOption('homeText');
 		
 		$analyse = new Analyse( $bdd, $manager, $model_analyse );
-		$analyses = $analyse->getItems( ['flag_accueil' => 1 ], false, 1, false, false );
+		$analyses = $analyse->getItems( ['flag_accueil' => 1 ], false, 1, false, $user->id_utilisateur );
 		
 		foreach( $analyses as $element ) {
 			echo $analyse->getDashboardHTML( $element );
