0af2390d0da5ff0f776ecbe56284317352aed692
benzouye <benzouye@hotmail.com>
2021-09-02 15:00:38 +0200

    Update ModelEtat.php

--- items/ModelEtat.php
+++ items/ModelEtat.php
@@ -2,13 +2,14 @@
 	$model_etat = (object) array(
 		'itemName' => 'etat',
 		'table' => DBPREF.'etat',
-		'single' => 'état inscription',
-		'plural' => 'états inscription',
+		'single' => 'état',
+		'plural' => 'états',
 		'orderby' => 'libelle',
 		'columns' => array(
 			(object) array(
 				'name' => 'id_etat',
 				'nicename' => 'ID',
+				'align' => 'center',
 				'grid' => (object) array(
 					'div' => 12,
 					'label' => 2,
@@ -23,6 +24,7 @@
 			(object) array(
 				'name' => 'libelle',
 				'nicename' => 'Libellé',
+				'align' => 'center',
 				'grid' => (object) array(
 					'div' => 12,
 					'label' => 2,
@@ -33,7 +35,26 @@
 				'editable' => true,
 				'required' => true,
 				'admin' => false
-			)
+			),
+			(object) array(
+				'name' => 'id_colorscheme',
+				'nicename' => 'Couleur',
+				'align' => 'center',
+				'grid' => (object) array(
+					'div' => 12,
+					'label' => 2,
+					'value' => 2,
+				),
+				'params' => array(
+					'type' => 'select',
+					'item' => 'colorscheme',
+					'columnKey' => 'id_colorscheme',
+					'columnLabel' => 'libelle'
+				),
+				'visible' => true,
+				'editable' => true,
+				'required' => true,
+				'admin' => false
+			),
 		)
 	);
-?>
